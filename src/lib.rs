5c6f6d40fa901e6b8bd487ee3e14ac2b460a3d27
cmeekva <58307385+cmeekva@users.noreply.github.com>
2025-04-07 12:29:33 -0400

    Working Categorize files and test

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,50 @@
 use glob::glob;
+use std::path::PathBuf;
 
-pub struct Config {
-    pub query: String,
-    pub file_path: String,
+#[derive(PartialEq, Debug)]
+pub enum LogType{
+    Csv,
+    Json,
+}
+
+#[derive(PartialEq, Debug)]
+pub struct LogFile {
+    pub log_type: LogType,
+    pub file_path: PathBuf,
 }
 
 pub fn iterate_through_input_dir(input_dir:String){
+
+    let mut paths: Vec<PathBuf> = Vec::new();
+
     for entry in glob(input_dir.as_str()).expect("Failed to read glob pattern") {
         match entry {
-            Ok(path) => println!("{:?}", path.display()),
+            Ok(path) => paths.push(path),
             Err(e) => println!("{:?}", e),
         }
     }
+
+    let supported_files = categorize_files(&paths);
+
+    for path in supported_files{
+        println!("{}", path.file_path.display())
+    }
+}
+
+pub fn categorize_files(file_paths: &Vec<PathBuf>) -> Vec<LogFile>{
+    let mut supported_files: Vec<LogFile> = Vec::new();
+
+    for file_path in file_paths{
+        if let Some(extension) = file_path.extension() {
+            if extension == "csv"{
+                supported_files.push(
+                    LogFile{
+                        log_type:LogType::Csv,
+                        file_path:file_path.to_path_buf(),
+                    }
+                )
+            }
+        }
+    }
+    supported_files
 }
---
+++ tests/file_categorization_tests.rs
@@ -0,0 +1,17 @@
+use log_checker::*;
+use std::path::PathBuf;
+
+#[test]
+fn categorizes_csvs() {
+    let mut paths: Vec<PathBuf> = Vec::new();
+    paths.push(PathBuf::from("/path/to/file1.txt"));
+    paths.push(PathBuf::from("/path/to/file2.csv"));
+
+    let result = categorize_files(&paths);
+    let expected: Vec<LogFile> = vec![LogFile{
+        log_type:LogType::Csv,
+        file_path:PathBuf::from("/path/to/file2.csv"),
+    }];
+
+    assert_eq!(result, expected);
+}
