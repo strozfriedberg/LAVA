3508ea4d14ee28fd65b0d807dcccf7732ce4a5d0
cmeekva <58307385+cmeekva@users.noreply.github.com>
2025-04-22 23:21:12 -0400

    got the time identification working for CSVs

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ use rayon::prelude::*;
 use csv::Writer;
 use serde::Serialize;
 use regex::Regex;
+use once_cell::sync::Lazy;
 // use polars::prelude::*;
 use csv::ReaderBuilder;
 
@@ -33,11 +34,52 @@ pub struct ProcessedLogFile {
     pub size: u64,
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(Debug)]
 pub struct DateRegex {
     pub date_format: String,
-    pub date_regex: Str,
+    pub date_regex: Regex,
 }
+
+pub static DATE_REGEXES: Lazy<Vec<DateRegex>> = Lazy::new(|| {
+    vec![
+        DateRegex {
+            date_format: "MM-DD-YYYY".to_string(),
+            date_regex: Regex::new(r"^\d{2}-\d{2}-\d{4}$").unwrap(),
+        },
+        DateRegex {
+            date_format: "YYYY-MM-DD".to_string(),
+            date_regex: Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap(),
+        },
+        DateRegex {
+            date_format: "DD-MM-YYYY".to_string(),
+            date_regex: Regex::new(r"^\d{2}-\d{2}-\d{4}$").unwrap(),
+        },
+        DateRegex {
+            date_format: "YYYY/MM/DD".to_string(),
+            date_regex: Regex::new(r"^\d{4}/\d{2}/\d{2}$").unwrap(),
+        },
+        DateRegex {
+            date_format: "MMM DD YYYY".to_string(), // e.g. Mar 22 2022
+            date_regex: Regex::new(r"^[A-Z][a-z]{2} \d{1,2} \d{4}$").unwrap(),
+        },
+        DateRegex {
+            date_format: "MMMM DD, YYYY".to_string(), // e.g. March 22, 2022
+            date_regex: Regex::new(r"^[A-Z][a-z]+ \d{1,2}, \d{4}$").unwrap(),
+        },
+        DateRegex {
+            date_format: "YYYY-MM-DD HH:MM:SS".to_string(), // 24-hour datetime
+            date_regex: Regex::new(r"^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$").unwrap(),
+        },
+        DateRegex {
+            date_format: "YYYY-MM-DDTHH:MM:SSZ".to_string(), // ISO 8601
+            date_regex: Regex::new(r"^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$").unwrap(),
+        },
+        DateRegex {
+            date_format: "M/D/YYYY H:MM AM/PM".to_string(), // 12-hour US time
+            date_regex: Regex::new(r"^\d{1,2}/\d{1,2}/\d{4} \d{1,2}:\d{2} (AM|PM|am|pm)$").unwrap(),
+        },
+    ]
+});
 
 pub fn iterate_through_input_dir(input_dir:String){
 
@@ -118,8 +160,12 @@ pub fn process_file(log_file: &LogFile) -> Result<ProcessedLogFile, Box<dyn Erro
 
     let (hash, size) = get_hash_and_size(&log_file.file_path)?; // The question mark here will propogate any possible error up.
     let file_name = log_file.file_path.file_name().expect("Error getting file name");
-    println!("{} has the hash {}!", file_name.to_string_lossy().to_string(), hash);
-    find_timestamp_field(log_file);
+
+    let (time_header, time_format) = find_timestamp_field(log_file)?;
+    println!(
+        "Match found Column '{}' matches the '{}' format in {}",
+        time_header, time_format, log_file.file_path.to_string_lossy().to_string()
+    );
     Ok(
         ProcessedLogFile{
             sha256hash: hash,
@@ -131,21 +177,25 @@ pub fn process_file(log_file: &LogFile) -> Result<ProcessedLogFile, Box<dyn Erro
 }
 
 
-pub fn find_timestamp_field(log_file: &LogFile) -> Result<(), Box<dyn Error>> { //This is lazy here
+pub fn find_timestamp_field(log_file: &LogFile) -> Result<(String, String), Box<dyn Error>> { //This is lazy here
     if log_file.log_type == LogType::Csv {
         let file = File::open(&log_file.file_path)?;
         let mut reader = ReaderBuilder::new()
             .has_headers(true) // Set to false if there's no header
             .from_reader(file);
 
-        let headers = reader.headers()?; // this returns a &StringRecord
-        println!("Headers: {:?}", headers);
-
-        let record = reader.records().next().unwrap(); // Read the first line
-        println!("{:?}", record);
+        let headers = reader.headers()?.clone(); // this returns a &StringRecord
+        let record = reader.records().next().unwrap()?; // This is returning a result, that is why I had to use the question mark below before the iter()
+        for (i, field) in record.iter().enumerate() {
+            for date_regex in DATE_REGEXES.iter() {
+                if date_regex.date_regex.is_match(field) {
+                    return Ok((headers.get(i).unwrap().to_string(), date_regex.date_format.clone()));//I know the clone is lazy I am just tired
+                }
+            }
+        }
     }
 
-    Ok(())
+    Err("No matching timestamp field found.".into())
 }
 // pub fn process_csv_file(log_file: &LogFile) -> ProcessedLogFile{
     
