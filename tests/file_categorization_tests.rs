87001ffb8a9c332fee87abe4cbb68b45927f0a5d
Colin Meek <colin.meek@aon.com>
2025-04-29 17:47:39 -0400

    ass json test

--- tests/file_categorization_tests.rs
+++ tests/file_categorization_tests.rs
@@ -7,11 +7,16 @@ use std::path::PathBuf;
 #[test]
 fn categorizes_csvs() {
     let mut paths: Vec<PathBuf> = Vec::new();
+    paths.push(PathBuf::from("/path/to/file1.json"));
     paths.push(PathBuf::from("/path/to/file1.txt"));
     paths.push(PathBuf::from("/path/to/file2.csv"));
 
     let result = categorize_files(&paths);
     let expected: Vec<LogFile> = vec![LogFile {
+        log_type: LogType::Json,
+        file_path: PathBuf::from("/path/to/file1.json"),
+    },
+        LogFile {
         log_type: LogType::Unstructured,
         file_path: PathBuf::from("/path/to/file1.txt"),
     },
