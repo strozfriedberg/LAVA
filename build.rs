e463afa9610cc07017402197b8a5c4fda3ff71de
Colin Meek <colin.meek@aon.com>
2025-05-14 21:50:06 -0400

    extract methods

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 use serde::Deserialize;
 use std::env;
+use std::ffi::OsString;
 use std::fmt;
 use std::fs;
 use std::path::Path;
@@ -18,13 +19,43 @@ impl fmt::Display for RawDateRegexWithTests {
     }
 }
 
+#[derive(Deserialize)]
+struct RawRedactionWithTests {
+    name: String,
+    pattern: String,
+    should_match: String,
+    should_not_match: String,
+}
+
+impl fmt::Display for RawRedactionWithTests {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}, {}", self.name, self.pattern)
+    }
+}
+
 fn main() {
-    let yaml_path = Path::new("regexes.yml");
-    let out_dir = env::var_os("OUT_DIR").unwrap();
-    let dest_path = Path::new(&out_dir).join("generated_regexes.rs");
-    let content = fs::read_to_string(yaml_path).expect("Failed to read YAML file");
+    let regex_yaml_path = Path::new("regexes.yml");
+    let content = fs::read_to_string(regex_yaml_path).expect("Failed to read YAML file");
     let parsed: Vec<RawDateRegexWithTests> =
         serde_yaml::from_str(&content).expect("Failed to parse YAML");
+
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+
+    generate_date_regex_vector(&parsed, &out_dir);
+    generate_date_regex_tests(&parsed, &out_dir);
+
+    generate_
+
+    println!("cargo:rerun-if-changed=redactions.yml");
+    println!("cargo:rerun-if-changed=regexes.yml");
+    println!("cargo:rerun-if-changed=build.rs");
+}
+
+
+fn generate_date_regex_vector(parsed: &Vec<RawDateRegexWithTests>, out_dir: &OsString) {
+
+    let dest_path = Path::new(out_dir).join("generated_date_regexes.rs");
+
     let mut generated_code = String::new();
     generated_code.push_str("use once_cell::sync::Lazy;\n");
     generated_code.push_str("use regex::Regex;\n");
@@ -33,7 +64,7 @@ fn main() {
         .push_str("pub static PREBUILT_DATE_REGEXES: Lazy<Vec<DateRegex>> = Lazy::new(|| {\n");
     generated_code.push_str("    vec![\n");
 
-    for entry in &parsed {
+    for entry in parsed {
         // Write each item in the vec
         generated_code.push_str(&format!(
             "        DateRegex {{\n            pretty_format: \"{}\".to_string(),\n            strftime_format: \"{}\".to_string(),\n            regex: Regex::new(r\"({})\").unwrap(),\n        }},\n",
@@ -45,9 +76,11 @@ fn main() {
 
     generated_code.push_str("    ]\n});\n");
 
-    fs::write(&dest_path, generated_code).expect("Failed to write generated_regexes.rs");
+    fs::write(&dest_path, generated_code).expect("Failed to write generated_date_regexes.rs");
+}
 
-    let test_out_path = Path::new(&std::env::var("OUT_DIR").unwrap()).join("generated_tests.rs");
+fn generate_date_regex_tests(parsed: &Vec<RawDateRegexWithTests>, out_dir: &OsString){
+    let test_out_path = Path::new(&out_dir).join("generated_date_tests.rs");
 
     let mut test_code = String::new();
     test_code.push_str("#[cfg(test)]\n");
@@ -75,7 +108,5 @@ fn main() {
 
     test_code.push_str("}\n");
 
-    fs::write(&test_out_path, test_code).expect("Failed to write generated_tests.rs");
-    println!("cargo:rerun-if-changed=regexes.yml");
-    println!("cargo:rerun-if-changed=build.rs");
+    fs::write(&test_out_path, test_code).expect("Failed to write generated_date_tests.rs");
 }
