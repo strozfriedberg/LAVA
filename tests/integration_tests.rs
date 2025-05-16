1f66d0e9e2640f0e25c8163bc289db022dd22558
Colin Meek <colin.meek@aon.com>
2025-05-16 12:47:36 -0400

    failing integration tests

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -48,13 +48,14 @@ fn integration_test_successful_run_no_errors(){
 
 
 #[test]
-fn integration_test_successful_run_duplicates(){
+fn integration_test_successful_run_duplicates_and_redactions(){
     let data = "\
     id,name,date\n\
     1,John,2025-05-09 10:00:00\n\
     2,Jane,2025-05-10 11:00:00\n\
     2,Jane,2025-05-10 11:00:00\n\
-    4,James,2025-06-01 13:00:00\n";
+    4,James,2025-06-01 13:00:00\n\
+    4,J*********s,2025-06-01 13:00:00\n";
 
     let temp_log_file = TempInputFile::new(LogType::Csv, data);
     let log_file = temp_log_file.get_log_file_object();
@@ -64,5 +65,50 @@ fn integration_test_successful_run_duplicates(){
     let processed = output.expect("Failed to get Proceesed Log File");
     assert_eq!(None, processed.error);
     assert_eq!("1", processed.num_dupes.unwrap());
+    assert_eq!("1", processed.num_redactions.unwrap());
+    temp_log_file.delete_temp_file();
+}
+
+#[test]
+fn integration_test_out_of_order_time_run_duplicates_and_redactions_1(){
+    let data = "\
+    id,name,date\n\
+    1,John,2025-05-09 10:00:00\n\
+    2,Jane,2025-05-10 10:30:00\n\
+    2,Jane,2025-05-10 10:15:00\n\
+    4,James,2025-06-01 13:00:00\n\
+    4,J*********s,2025-06-01 13:00:00\n";
+
+    let temp_log_file = TempInputFile::new(LogType::Csv, data);
+    let log_file = temp_log_file.get_log_file_object();
+    let settings = ExecutionSettings::create_integration_test_object(None, false);
+
+    let output = process_file(log_file, &settings);
+    let processed = output.expect("Failed to get Proceesed Log File");
+    assert_ne!(None, processed.error);
+    assert_eq!("1", processed.num_dupes.unwrap());
+    assert_eq!("1", processed.num_redactions.unwrap());
+    temp_log_file.delete_temp_file();
+}
+
+#[test]
+fn integration_test_out_of_order_time_run_duplicates_and_redactions_2(){
+    let data = "\
+    id,name,date\n\
+    1,John,2025-05-09 10:00:00\n\
+    2,Jane,2025-05-10 10:30:00\n\
+    2,Jane,2025-05-10 10:45:00\n\
+    4,James,2025-06-01 13:00:00\n\
+    4,J*********s,2025-06-01 10:00:00\n";
+
+    let temp_log_file = TempInputFile::new(LogType::Csv, data);
+    let log_file = temp_log_file.get_log_file_object();
+    let settings = ExecutionSettings::create_integration_test_object(None, false);
+
+    let output = process_file(log_file, &settings);
+    let processed = output.expect("Failed to get Proceesed Log File");
+    assert_ne!(None, processed.error);
+    assert_eq!("1", processed.num_dupes.unwrap());
+    assert_eq!("1", processed.num_redactions.unwrap());
     temp_log_file.delete_temp_file();
 }
