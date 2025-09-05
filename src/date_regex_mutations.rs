014aceffbb645adb1cb489feff05d9ef2bbcb72b
Colin Meek <colin.meek@aon.com>
2025-09-05 12:11:22 -0400

    implement the stripping to 10 chars

---
+++ src/date_regex_mutations.rs
@@ -0,0 +1,34 @@
+
+fn strip_to_10_most_significant_digits(input_string: &str) -> &str {
+    if input_string.len() > 10 {
+        &input_string[..10]
+    } else {
+        input_string
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_strip_more_than_10() {
+        let input = "1234567890123";
+        let output = strip_to_10_most_significant_digits(input);
+        assert_eq!(output, "1234567890")
+    }
+
+    #[test]
+    fn test_strip_exactly_10() {
+        let input = "1234567890";
+        let output = strip_to_10_most_significant_digits(input);
+        assert_eq!(output, "1234567890")
+    }
+
+    #[test]
+    fn test_strip_less_than_10() {
+        let input = "12345";
+        let output = strip_to_10_most_significant_digits(input);
+        assert_eq!(output, "12345")
+    }
+}
